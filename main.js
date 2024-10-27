db547d1abab6a680da9903b84895bee4bc11fdb7
unnamed755 <xoliboyevdoniyor@gmail.com>
2024-10-27 21:46:51 +0500

    bot

---
+++ main.js
@@ -0,0 +1,9 @@
+const username = prompt ("sting typedagi malumot kiriting")
+const userage = +prompt ("number typedagi malumot kiriting")
+const namee = prompt ("boolean typedagi malumot kiriting")
+alert("hamma malumotlarni to'g'ri kiritganingizga ishonchingiz komilmi?")
+alert(`
+    ${username} so'zi ${typeof username} tipiga teng
+    ${userage} raqami ${typeof userage} tipiga teng
+    ${namee} so'zi ${typeof namee} tipiga teng`);
+
